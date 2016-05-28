11e6534e3b68803d997ac3beb0f17c7206ca3cde
Zach Parrish <zach.parrish@yahoo.com>
2016-05-28 18:06:52 -0500

    Added NETCore projects

---
+++ MigraDoc/src/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Threading.Tasks;
+using MigraDoc.DocumentObjectModel;
+using MigraDoc.Rendering;
+using MigraDoc.Rendering.Resources;
+using PdfSharp.Pdf;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            //var _resmngr = new ResourceManager("MigraDoc.Rendering.Resources.Messages", typeof(Area).GetTypeInfo().Assembly);
+
+            //var resources = typeof(Document).GetTypeInfo().Assembly.GetManifestResourceNames();
+
+            //try
+            //{
+            //    var ci = new System.Globalization.CultureInfo("de");
+            //    var x = _resmngr.GetString("NumberTooLargeForRoman", ci);
+            //}
+            //catch (Exception ex)
+            //{
+
+            //    throw;
+            //}
+
+
+            //var y = "stop";
+        }
+    }
+}
